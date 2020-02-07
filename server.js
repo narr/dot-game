5514e200ba89b7949fdfd64f375a01594de10829
narr <nardgu@gmail.com>
2020-02-07 11:04:56 +0800

    add npm script for CI

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get("/", function (request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 8000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
